cd631b07bbed23c419eeaf0cb358420c133eef0e
lethanhdat762003 <dat.le@digitalfortress.dev>
2025-12-29 21:37:05 +0700

    feat: add isEnabled flag for opentelemetry

--- internal/config/config.go
+++ internal/config/config.go
@@ -164,7 +164,7 @@ func setDefaults(vp *viper.Viper) {
 	vp.SetDefault("protocols.helium.enabled", false)
 
 	// OpenTelemetry defaults
-	vp.SetDefault("opentelemetry.enabled", true)
+	vp.SetDefault("opentelemetry.enabled", false)
 	vp.SetDefault("opentelemetry.endpoint", "signoz-otel-collector:4317")
 	vp.SetDefault("opentelemetry.environment", "development")
 	vp.SetDefault("opentelemetry.sampling_ratio", 1.0)
